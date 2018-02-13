3509841718e15bf7a63adf4accc7e0491e8b96cf
Alison Wood <alisonwood29@gmail.com>
2018-02-13 16:59:16 +0000

    added function to add dinosaur to enclosure, test passing

--- park.js
+++ park.js
@@ -6,4 +6,8 @@ Park.prototype.dinosaurCount = function () {
   return this.enclosure.length;
 }
 
+Park.prototype.addDinosaur = function (dinosaur) {
+  this.enclosure.push(dinosaur);
+}
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -5,9 +5,11 @@ const Dinosaur = require('../dinosaur');
 describe('Park', function () {
 
   let park;
+  let dinosaur;
 
   beforeEach(function () {
     park = new Park();
+    dinosaur = new Dinosaur('Stegasaurus', 2)
   })
 
   it('should have an enclosure', function () {
@@ -18,5 +20,10 @@ describe('Park', function () {
     const actual = park.dinosaurCount();
     assert.strictEqual(actual, 0);
   })
+
+  it('should be able to add a dinosaur', function () {
+    park.addDinosaur(dinosaur);
+    assert.deepStrictEqual(park.enclosure, [dinosaur])
+  } )
 
 })
